7ecd9b55073adcda3ff073190255aa5cec74cbca
huraira <hurairabaloch996@gmail.com>
2023-02-15 11:06:55 +0500

    "my firsr repository 2nd semster"

---
+++ my.js
@@ -0,0 +1,16 @@
+const close = document.querySelector(".close");
+const open = document.querySelector(".ham");
+const menu = document.querySelector(".menu");
+close.addEventListener("click", () => {
+  menu.style.visibility = "hidden";
+});
+open.addEventListener("click", () => {
+  menu.style.visibility = "visible";
+});
+
+let click =
+    
+
+click.addEventListener("info"); 
+
+    alert("item added to cart");
